df4bc7cd0751190f1ae8f90f2394319868947f3d
VitorAlvess <53319601+VitorAlvess@users.noreply.github.com>
2022-11-29 13:25:46 -0300

    Estilização da página Start

--- src/Pages/Start/index.jsx
+++ src/Pages/Start/index.jsx
@@ -1,15 +1,35 @@
 import React from "react";
-import { View, ScrollView, Text, Image } from "react-native";
+import { View, ScrollView, Text, Image, StyleSheet } from "react-native";
 
 export default function Start(){
 return(
-        <View>
+        <View style={styles.container}>
             <ScrollView showVerticalScrollIndicator={false}>
-            <View>
-                <Image source={require("../../assets/icons/logo.png")} />
-                <Text>Vamos transformar sua vida {"/n"} em jogo, buscando sempre {"/n"} o melhor nível </Text>
+            <View style={{alignItems: 'center'}}>
+                <Image source={require("../../assets/icons/logo3.png")} style={styles.logo} />
+                <Text style={styles.description}>Vamos transformar sua vida {"\n"} em jogo, buscando sempre {"\n"} o melhor nível </Text>
             </View>
             </ScrollView>
         </View>
     )
 }
+// Legal isso de definir os styles aqui em baixo
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'rgba(21, 21, 21, 0.98)',
+    },
+
+    logo: {
+        width: 300,
+        height: 60,
+        marginTop: 60,
+        marginBottom: 20,
+    },
+    description: {
+        color: '#FFFFFF',
+        fontSize: 20,
+        textAlign: 'center',
+        marginVertical: 60,
+    },
+})
