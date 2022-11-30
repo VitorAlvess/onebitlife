0b12197959c452fa43d1456af7c41cf62b30a1d1
VitorAlvess <53319601+VitorAlvess@users.noreply.github.com>
2022-11-30 19:36:07 -0300

    Estruturas das rotas e da homepage

--- src/Routes/AllPages.jsx
+++ src/Routes/AllPages.jsx
@@ -4,6 +4,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Start from "../Pages/Start";
 import AppExplanation from '../Pages/AppExplanation'
 import Home from "../Pages/home";
+import HabitPage from "../Pages/HabitPage";
 const Stack = createNativeStackNavigator();
 
 export default function AllPages(){
@@ -16,6 +17,8 @@ export default function AllPages(){
                 <Stack.Screen name="Start" component={Start} />
                 <Stack.Screen name="AppExplanation" component={AppExplanation} />
                 <Stack.Screen name="Home" component={Home} />
+                <Stack.Screen name="HabitPage" component={HabitPage} />
+
 
             </Stack.Navigator>   
         </NavigationContainer> 
--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -1,5 +1,7 @@
+import { useState } from "react";
 import AllPages from "./AllPages";
-
+import HomePage from "./HomePage";
 export default function Router(){
-    return <AllPages />
+    const { showHome, setShowHome} = useState('false')
+    return <>{showHome === 'True' ? <HomePage /> : <AllPages />}</>
 }
