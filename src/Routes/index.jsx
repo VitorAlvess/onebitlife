702f834be66d2993b30adeb430a762aa47e4954f
VitorAlvess <53319601+VitorAlvess@users.noreply.github.com>
2022-12-01 15:59:07 -0300

    Criando serviço de SET

--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -4,7 +4,7 @@ import AllPages from "./AllPages";
 import HomePage from "./HomePage";
 
 export default function Routes() {
-  const [showHome, setShowHome] = useState("true");
+  const [showHome, setShowHome] = useState("false");
 
   return <>{showHome === "true" ? <HomePage /> : <AllPages />}</>;
 }
