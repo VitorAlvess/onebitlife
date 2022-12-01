7d6e666040ad241dbc68f5a8db22fc75df9c2d89
VitorAlvess <53319601+VitorAlvess@users.noreply.github.com>
2022-12-01 16:09:29 -0300

    Criando serviço de SET

--- src/Pages/AppExplanation/index.jsx
+++ src/Pages/AppExplanation/index.jsx
@@ -1,15 +1,32 @@
+import React, { useState } from "react";
 import { useNavigation } from "@react-navigation/native";
-import React from "react";
 import { ScrollView, StyleSheet, Text, View, handleNavHome } from "react-native";
 import DefaulButton from "../../common/DefaultButton";
 import ExplanationCard from "../../common/explanation/ExplanationCard";
+import ChangeNavigationService from "../../services/ChangeNavigationService";
 export default function AppExplanation(){
     const navigation = useNavigation()
+    const [showHome, setShowHome] = useState("false");
+    const startDate = new Date();
+    const appStartData = `${startDate.getFullYear()}-${startDate.getMonth()}-${startDate.getDate()}`;
 
     function handleNavHome(){
         navigation.navigate('Home')
     }
 
+    function handleSetShowHome() {
+        if (showHome !== "true") {
+          ChangeNavigationService.setShowHome({ showHome: "true", appStartData })
+            .then(() => console.log(`Sucesso! ${showHome} ${appStartData}`))
+            .catch((err) => console.log(err));
+          setShowHome("true");
+    
+          handleNavHome();
+        }
+    
+        handleNavHome();
+      }
+    
 
     return( 
         <View style={styles.container}>
@@ -25,7 +42,7 @@ export default function AppExplanation(){
                     <Text style={styles.description}>
                         Na próxima tela você vai poder escolher {'\n'} seus 4 hábitos de forma individual
                     </Text>
-                    <DefaulButton buttonText={'Continuar'} handlePress={handleNavHome} width={250} height={50}/>
+                    <DefaulButton buttonText={'Continuar'} handlePress={handleSetShowHome} width={250} height={50}/>
                 </View>
             </ScrollView>
         </View>
